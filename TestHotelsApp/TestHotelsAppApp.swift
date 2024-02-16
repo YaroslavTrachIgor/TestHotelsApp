6e13cd4ccd2908f06e68944e4bc4521223bb4143
Yaroslav Trach <zhbr282@gmail.com>
2024-02-15 22:01:17 -0500

    Initial Commit

---
+++ TestHotelsApp/TestHotelsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestHotelsAppApp.swift
+//  TestHotelsApp
+//
+//  Created by User on 2024-02-15.
+//
+
+import SwiftUI
+
+@main
+struct TestHotelsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
